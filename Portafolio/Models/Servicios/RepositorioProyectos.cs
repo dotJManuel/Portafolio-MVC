c92e719f51c4e034d7925b1b9d7cb5aafcb6a6fa
dotJManuel <jsmnlhm@gmail.com>
2023-09-15 17:45:17 -0600

    Inicial

---
+++ Portafolio/Models/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,45 @@
+﻿namespace Portafolio.Models.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>
+            {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "https://amazon.com",
+                    ImagenURL = "/imagenes/amazon.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Pagina de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/imagenes/nyt.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/imagenes/reddit.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en linea para comprar videojuegos",
+                    Link = "https://store.steampowered.com",
+                    ImagenURL = "/imagenes/steam.PNG"
+                }
+            };
+        }
+    }
+}
